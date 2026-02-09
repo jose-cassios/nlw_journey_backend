b70f33baaa994efa472a5c14850af4098dcc99e9
jose-cassios <cassios.torres.010@gmail.com>
2026-02-09 11:37:43 -0300

    chore(prisma): upgrade to Prisma v7 and migrate to adapter-based configuration

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,5 +1,11 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client'
+import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3'
+
+const adapter = new PrismaBetterSqlite3({
+  url: 'file:./dev.db',
+})
 
 export const prisma = new PrismaClient({
-    log: ['query'],
+  adapter,
+  log: ['query'],
 })
