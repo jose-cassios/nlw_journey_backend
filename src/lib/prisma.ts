6817967744b5003998bf401cf29faad3d8a4b24e
jose-cassios <cassios.torres.010@gmail.com>
2026-02-28 14:17:25 -0300

    build: fix 3

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,11 +1,3 @@
 import { PrismaClient } from '@prisma/client'
-import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3'
 
-const adapter = new PrismaBetterSqlite3({
-  url: 'file:./dev.db',
-})
-
-export const prisma = new PrismaClient({
-  adapter,
-  log: ['query'],
-})
+export const prisma = new PrismaClient()
